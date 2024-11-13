72e7a1aab075fcd8dfcaab87579dbd2f4950701f
Elcale1703 <m.agudeloo@uniandes.edu.co>
2024-11-13 12:23:28 -0500

    Falta navegacion mobil

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,10 +1,11 @@
 "use server";
 
 import { ID, Query } from "node-appwrite";
-import { createAdminClient } from "../appwrite";
+import { createAdminClient, createSessionClient } from "../appwrite";
 import { appwriteConfig } from "../appwrite/config";
 import { parseStringify } from "../utils";
 import { cookies } from "next/headers";
+import { avatarPlaceholderUrl } from "@/constants";
 
 const getUserByEmail = async (email: string) => {
     const { database } = await createAdminClient();
@@ -49,13 +50,13 @@ export const createAccount = async ({ fullName, email }: { fullName: string; ema
             {
                 fullName,
                 email,
-                avatar: "https://img.freepik.com/psd-gratis/ilustracion-3d-avatar-o-perfil-humano_23-2150671142.jpg",
+                avatar: avatarPlaceholderUrl,
                 accountId,
             }
         );
     }
     return parseStringify({ accountId });
-}
+};
 
 export const verifySecret = async ({ accountId, password }: { accountId: string; password: string }) => {
     try {
@@ -74,4 +75,20 @@ export const verifySecret = async ({ accountId, password }: { accountId: string;
     } catch (error) {
         handleError(error, "Failed to verify OTP");
     }
-}
+};
+
+export const getCurrentUser = async () => {
+    const { database, account } = await createSessionClient();
+
+    const result = await account.get();
+
+    const user = await database.listDocuments(
+        appwriteConfig.databaseId,
+        appwriteConfig.userscollectionId,
+        [Query.equal("accountId", result.$id)]
+    );
+
+    if (user.total < 0) return null;
+
+    return parseStringify(user.documents[0]);
+};
