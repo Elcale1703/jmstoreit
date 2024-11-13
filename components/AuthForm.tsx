cc651317cb8017ad99902a65e229319f13471d2a
Elcale1703 <m.agudeloo@uniandes.edu.co>
2024-11-12 23:41:04 -0500

    Completo OTP

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -17,6 +17,7 @@ import { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { createAccount } from "@/lib/actions/user.actions";
+import OTPModal from "./OTPModal";
 
 const authFormSchema = (formType: FormType) => {
     return z.object({
@@ -122,6 +123,7 @@ const AuthForm = ({ type }: { type: FormType }) => {
                     </div>
                 </form>
             </Form>
+            {accountId && <OTPModal email={form.getValues("Email")} accountId={accountId} />}
         </>
     );
 };
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -4,6 +4,7 @@ import { ID, Query } from "node-appwrite";
 import { createAdminClient } from "../appwrite";
 import { appwriteConfig } from "../appwrite/config";
 import { parseStringify } from "../utils";
+import { cookies } from "next/headers";
 
 const getUserByEmail = async (email: string) => {
     const { database } = await createAdminClient();
@@ -36,9 +37,9 @@ export const sendEmailOTP = async ({ email }: { email: string }) => {
 export const createAccount = async ({ fullName, email }: { fullName: string; email: string }) => {
     const existingUser = await getUserByEmail(email);
     const accountId = await sendEmailOTP({ email });
-    if(!accountId) throw new Error("Failed to send OTP");
+    if (!accountId) throw new Error("Failed to send OTP");
 
-    if(!existingUser) {
+    if (!existingUser) {
         const { database } = await createAdminClient();
 
         await database.createDocument(
@@ -54,4 +55,23 @@ export const createAccount = async ({ fullName, email }: { fullName: string; ema
         );
     }
     return parseStringify({ accountId });
+}
+
+export const verifySecret = async ({ accountId, password }: { accountId: string; password: string }) => {
+    try {
+        const { account } = await createAdminClient();
+
+        const session = await account.createSession(accountId, password);
+
+        (await cookies()).set("appwrite-session", session.secret, {
+            path: "/",
+            httpOnly: true,
+            sameSite: "strict",
+            secure: true,
+        });
+
+        return parseStringify({ sessionId: session.$id });
+    } catch (error) {
+        handleError(error, "Failed to verify OTP");
+    }
 }
